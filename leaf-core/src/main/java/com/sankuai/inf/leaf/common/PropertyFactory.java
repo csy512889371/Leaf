0947eea25ef75c716fdcafae2ab5f7305b8b9d6a
chenshiying <512889371@qq.com>
2020-03-02 15:34:29 +0800

    【修改】jar 版本

--- leaf-core/src/main/java/com/sankuai/inf/leaf/common/PropertyFactory.java
+++ leaf-core/src/main/java/com/sankuai/inf/leaf/common/PropertyFactory.java
@@ -3,19 +3,20 @@ package com.sankuai.inf.leaf.common;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Properties;
 
 public class PropertyFactory {
     private static final Logger logger = LoggerFactory.getLogger(PropertyFactory.class);
     private static final Properties prop = new Properties();
+
     static {
         try {
             prop.load(PropertyFactory.class.getClassLoader().getResourceAsStream("leaf.properties"));
-        } catch (IOException e) {
-            logger.warn("Load Properties Ex", e);
+        } catch (Exception e) {
+            logger.warn("Load leaf.properties Properties Exception");
         }
     }
+
     public static Properties getProperties() {
         return prop;
     }
